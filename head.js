3ab51cc45427e82f033d8f758e5e12701e105049
BrianKendalBucek <bbucek@gmail.com>
2022-07-13 21:24:52 +0000

    Added new function to return first index of array

---
+++ head.js
@@ -0,0 +1,23 @@
+const assertEqual = (actual, expected) => {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return;
+  }
+
+  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("Bootcamp", "Bootcamp");
+
+const head = (arr) => {
+  var first = arr[0];
+  return first;
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 5);
+assertEqual(head([1]), 1);
