c2d2e317b2698de6388c77fa4aa9e91d30414b65
BrianKendalBucek <bbucek@gmail.com>
2022-07-13 20:42:35 +0000

    Brian Buceks solution to assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = (actual, expected) => {
+  if (actual !== expected) {
+    console.log("🛑🛑🛑" + " Assertion Failed: " + actual + " !== " + expected);
+    return;
+  }
+
+  console.log("✅✅✅ " + " Assertion Passed: " + actual + " === " + expected);
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("Bootcamp", "Bootcamp");
