edcb0b2ae2dd3e5b33d0b333c988ab9bd0918d27
BrianKendalBucek <bbucek@gmail.com>
2022-07-16 19:12:59 +0000

    Created function to call eqArrays function and return an appropriate statement

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+function eqArrays(arr1, arr2) {
+  if(arr1.length !== arr2.length){
+    return false;
+  }
+  for(let i = 0; i < arr1.length; i++){
+    if(arr1[i] !== arr2[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+function assertArraysEqual(arr1, arr2) {
+  if(eqArrays(arr1, arr2)){
+    console.log("They are equal");
+  } else {
+    console.log("They are not equal");
+  }
+}
+
+// assertArraysEqual([1, 2, 3], [1, 2, 3])
+// assertArraysEqual([1, 2, 3], [1, 2, 4])
+// assertArraysEqual([1, 2, 3], [1, 2, 3, 4])
