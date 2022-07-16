b08dd419a536128de1703d1be455c7452a3740de
BrianKendalBucek <bbucek@gmail.com>
2022-07-16 15:38:23 +0000

    Function for comparing arrays

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = (actual, expected) => {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return;
+  }
+
+  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+};
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual(1, 2);
+// assertEqual("Bootcamp", "Bootcamp");
+
+
+
+
+//function to compare 2 arrays for a boolean result.
+function eqArrays(arr1, arr2) {
+  if(arr1.length !== arr2.length){
+    return false;
+  }
+  for(let i = 0; i < arr1.length; i++){
+    if(arr1[i] !== arr2[i]){
+      return false;
+    }
+  }
+  return true;
+}
+// console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), true);
