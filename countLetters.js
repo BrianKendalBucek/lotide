16ac64781c8776b69ddc28462472ff39803b58b9
BrianKendalBucek <bbucek@gmail.com>
2022-07-18 22:19:31 +0000

    Count letters of string

---
+++ countLetters.js
@@ -0,0 +1,34 @@
+const assertEqual = (actual, expected) => {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return;
+  }
+
+  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+};
+
+//Create a function called countLetters
+//should take in a sentence (as a string)
+//and return a count of each of the letters in that sentence
+
+const countLetters = str => {
+  const results = {};
+
+  for (const char of str) {
+
+    if (char === ' ') {
+      continue;
+    }
+
+    if (!results[char]) {
+      results[char] = 0;
+    }
+
+    results[char] ++;
+  }
+  return results;
+};
+
+// const result = countLetters("lighthouse in the house")
+// console.log(result);
+// assertEqual(result.l, 1);
