79365a52fbbe505d3ebbf45e8f3fbefe5c99bc9c
BrianKendalBucek <bbucek@gmail.com>
2022-07-25 03:58:02 +0000

    find key

--- findKey.js
+++ findKey.js
@@ -7,43 +7,19 @@
 //   console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
 // };
 
-// Not too long ago, we implemented a findKeyByValue function in Lotide. 
-// It was similar to, but more limited than, findKey from Lodash. 
-// Now that we know about callbacks, let's make a more complex version of this in a similar way to what Lodash has.
-// Again this challenge will help you practice your comfort with crafting higher-order functions that accept a callback.
-
-//Implement the function findKey which takes in an object and a callback. 
-//It should scan the object and return the first key for which the callback returns a truthy value. 
-//If no key is found, then it should return undefined.
+const findKey = (object, callback) => {
+  for (let key in object) {
+    if (callback(object[key])) return key;
+  }
+};
 
 //EXAMPLE
 
-// findKey({
+// assertEqual(findKey({
 //   "Blue Hill": { stars: 1 },
 //   "Akaleri":   { stars: 3 },
 //   "noma":      { stars: 2 },
 //   "elBulli":   { stars: 3 },
 //   "Ora":       { stars: 2 },
 //   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2) // => "noma"
-
-//Feel free to take a look at our solution for findKeyByValue. 
-//Some of the core logic will be the same (such as the looping over object keys part).
-
-//FIND KEY BY VALUE
-
-// const tvShows = {
-//   romance: "ER",
-//   drama: "The Wire",
-//   comedy: "Dumber"
-// };
-
-// const findKeyByValue = (obj, find) => {
-  
-//   for (const key of Object.keys(obj)) {
-//     const value = obj[key];
-//     if (value === find) {
-//       return key;
-//     }
-//   }
-// };
+// }, x => x.stars === 3), "Akaleri"); // => "Akaleri"
