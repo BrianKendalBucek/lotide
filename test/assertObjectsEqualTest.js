91d836b6c4feb29a2c6b6a1496a32afb4c092b88
BrianKendalBucek <bbucek@gmail.com>
2022-08-06 15:40:58 +0000

    commented out tests for assertArrays, Equal, and Objects to only return Mocha and Chai tests for main functions

--- test/assertObjectsEqualTest.js
+++ test/assertObjectsEqualTest.js
@@ -1,11 +1,11 @@
 const assertObjectsEqual = require('../assertObjectsEqual');
 const eqObjects = require('../eqObjects');
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
 
-const cd = { a: "1", b: "2", c: "3" };
-const dc = { b: "2", a: "1", c: "4" };
+// const cd = { a: "1", b: "2", c: "3" };
+// const dc = { b: "2", a: "1", c: "4" };
 
-assertObjectsEqual(eqObjects(ab, ba), true);
-assertObjectsEqual(eqObjects(cd, dc), false);
+// assertObjectsEqual(eqObjects(ab, ba), true);
+// assertObjectsEqual(eqObjects(cd, dc), false);
