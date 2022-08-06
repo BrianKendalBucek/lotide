60ba2341f3a0fc045e88660220a8d78c5c5f83b3
BrianKendalBucek <bbucek@gmail.com>
2022-08-06 19:12:19 +0000

    modified eqObjects and removed test that is meant for future assignment requiring recursion

--- eqObjects.js
+++ eqObjects.js
@@ -1,13 +1,19 @@
+const eqArrays = require('./eqArrays');
+
 const eqObjects = function(object1, object2) {
-  if(Object.entries(object1).length !== Object.entries(object2).length) {
+  if (Object.entries(object1).length !== Object.entries(object2).length) {
     return false;
   }
   for (const check of Object.entries(object1)) {
-    if(object2[check[0]] === object1[check[0]]){
-      return true;
+    console.log(check);
+    if (Array.isArray(object2[check[0]]) && Array.isArray(object1[check[0]])) {
+      return eqArrays(object1[check[0]], object2[check[0]]);
+    } else if(object2[check[0]] !== object1[check[0]]) {
+      return false;
     }  
   }
-  return false;
+  return true;
+
 };
 
 module.exports = eqObjects;
--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -1,8 +1,6 @@
 const assert = require('chai').assert;
 const eqObjects = require('../eqObjects');
 
-// { c: "1", d: ["2", 3] } dc = { d: ["2", 3], c: "1" }
-
 describe("#eqObjects", () => {
   it("Should return true when passed eqObjects({ a: 1, b: 2 }, { b: 2, a: 1 })", () => {
     assert.strictEqual(eqObjects({ a: 1, b: 2 }, { b: 2, a: 1 }), true);
@@ -19,10 +17,13 @@ describe("#eqObjects", () => {
   it("Should return true when passed eqObjects({ a: 1, b: [2, 3] }, { b: [2, 3], a: 1 })", () => {
     assert.strictEqual(eqObjects({ a: 1, b: [2, 3] }, { b: [2, 3], a: 1 }), true);
   });
-  it("Should return true when passed eqObjects({ a: { c: 1 }, b: 2 }, { a: { c: 1 }, b: 2 })", () => {
-    assert.strictEqual(eqObjects({ a: { c: 1 }, b: 2 }, { a: { c: 1 }, b: 2 }), true);
-  });
-  it("Should return true when passed eqObjects({ c: '1', d: ['2', 3] } dc = { d: ['2', 3], c: '1' })", () => {
+  it("Should return true when passed eqObjects({ c: '1', d: ['2', 3] }, { d: ['2', 3], c: '1' })", () => {
     assert.strictEqual(eqObjects({ c: "1", d: ["2", 3] }, { d: ["2", 3], c: "1" }), true);
   })
+  it("Should return false when passed eqObjects({ c: '1', d: ['2', 3] }, { c: '1', d: ['2;, '3'] })", () => {
+    assert.strictEqual(eqObjects({ c: "1", d: ["2", 3] }, { c: "1", d: ["2", "3"] }), false);
+  })
+  // it("Should return true when passed eqObjects({ a: { c: 1 }, b: 2 }, { a: { c: 1 }, b: 2 })", () => {
+  //   assert.strictEqual(eqObjects({ a: { c: 1 }, b: 2 }, { a: { c: 1 }, b: 2 }), true);
+  // }); Will use this test when doing the stretch project requiring recursion for nested objects check.
 });
